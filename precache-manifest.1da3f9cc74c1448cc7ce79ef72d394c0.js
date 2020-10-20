6f434d9a745927b4b4bba611cf8d5fa9b3d30fa0
Claudio Palma López <xclaudio@outlook.com>
2020-10-20 14:21:35 -0300

    Updates

---
+++ precache-manifest.1da3f9cc74c1448cc7ce79ef72d394c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca51a4e6389dd38a8b9dd223d9d3ec1",
+    "url": "/react-midoriweb/index.html"
+  },
+  {
+    "revision": "49bea9fc2651fff0d8c4",
+    "url": "/react-midoriweb/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "772be875ec4b903735e8",
+    "url": "/react-midoriweb/static/js/2.320d0581.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-midoriweb/static/js/2.320d0581.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49bea9fc2651fff0d8c4",
+    "url": "/react-midoriweb/static/js/main.83d6c058.chunk.js"
+  },
+  {
+    "revision": "acbe5546d7c55f6fcbe4",
+    "url": "/react-midoriweb/static/js/runtime-main.af09489a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-midoriweb/static/media/logo.5d5d9eef.svg"
+  }
+]);
